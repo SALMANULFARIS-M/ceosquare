0d2cbd2e3ee2b698164c932513da7157bcb71b3f
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-12-16 14:47:03 +0530

    Added new blog as 4th one

--- src/assets/blogs/index.ts
+++ src/assets/blogs/index.ts
@@ -1,9 +1,11 @@
 import blog1 from './datas/blog-1.json';
 import blog2 from './datas/blog-2.json';
 import blog3 from './datas/blog-3.json';
+import blog4 from './datas/blog-4.json';
 
 export const blogsData = [
   blog1,
   blog2,
-  blog3
+  blog3,
+  blog4
 ];
